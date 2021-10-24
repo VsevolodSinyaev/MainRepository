7e8eea065d0a19c764df83e4b8fe5ced5217c60d
Vsevolod Sinyaev <vsevolods3003@gmail.com>
2021-10-24 10:33:17 +0300

    interface WAllhavenApi

--- app/src/main/java/com/vss3003/wallpapersearcher/MainActivity.kt
+++ app/src/main/java/com/vss3003/wallpapersearcher/MainActivity.kt
@@ -2,6 +2,8 @@ package com.vss3003.wallpapersearcher
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import retrofit2.http.GET
+import retrofit2.http.Query
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -9,3 +11,13 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
     }
 }
+
+interface WallhavenApi {
+    @GET("https://www.googleapis.com/books/v1/volumes?q=flowers&orderBy=toplist&key=LAq8Y9XtyC3vD5HH7AVjzzQUi4TZE8pG")
+    fun getWallpapers(
+        @Query("api_key") apiKey: String,
+        @Query("page") page: Int,
+        @Query("category") category: String,
+    )
+}
+
