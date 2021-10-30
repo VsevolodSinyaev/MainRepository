4140f9df2257829ec1830cadc55d3fca3a3d6990
Vsevolod Sinyaev <vsevolods3003@gmail.com>
2021-10-30 15:52:17 +0300

    RecyclerAdapter gradle files

---
+++ app/src/main/java/com/vss3003/wallpapersearcher/CharacterListRecyclerAdapter.kt
@@ -0,0 +1,44 @@
+package com.vss3003.wallpapersearcher
+
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import kotlinx.android.synthetic.main.character_item.view.*
+
+class CharacterListRecyclerAdapter(private val clickListener: OnItemClickListener) : RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+    private val items: MutableList<Character> = mutableListOf()
+
+    override fun getItemCount() = items.size
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return CharacterViewHolder(LayoutInflater.from(parent.context).inflate(R.layout.character_item, parent, false))
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        when (holder) {
+            is CharacterViewHolder -> {
+                holder.bind(items[position])
+                holder.itemView.item_container.setOnClickListener {
+                    clickListener.click(items[position])
+
+                }
+
+            }
+
+        }
+
+    }
+
+    fun addItems(list: List<Character>) {
+        items.clear()
+        items.addAll(list)
+        notifyDataSetChanged()
+
+    }
+
+    interface OnItemClickListener {
+        fun click(character: Character)
+
+    }
+}
